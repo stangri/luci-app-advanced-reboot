dd7512e3ae63c2bbf2928bac4044dd646ada7b4f
Stan Grishin <stangri@melmac.ca>
2025-08-26 03:36:19 +0000

    fix zyxel errors

--- htdocs/luci-static/resources/view/system/advanced-reboot.js
+++ htdocs/luci-static/resources/view/system/advanced-reboot.js
@@ -270,7 +270,7 @@ Click "Proceed" below to reboot device to the selected partition.'
 								{
 									class: "btn cbi-button cbi-button-positive important",
 									click: L.bind(function () {
-										this.callBootPartition({ number: Number(pn) })
+										this.callBootPartition({ number: String(pn) })
 											.then(
 												L.bind(function (res) {
 													ui.hideModal();
